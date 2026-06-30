04d7714492c867f9934d289b3e5903217d4674d6
IraTyagii <iramybooksays@gmail.com>
2026-06-30 11:50:06 +0530

    Create tb_pkg package with included class files

---
+++ tb/tb_pkg.sv
@@ -0,0 +1,14 @@
+package tb_pkg;
+
+    // Include all class files
+
+    `include "transaction.sv"
+    `include "generator.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "reference_model.sv"
+    `include "scoreboard.sv"
+    `include "environment.sv"
+    `include "test.sv"
+
+endpackage
